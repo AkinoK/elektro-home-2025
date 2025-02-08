d00d1b1faa32f558e06718d7e3184a2f6b537c85
AkinoK <84216541+AkinoK@users.noreply.github.com>
2025-02-08 13:55:57 -0700

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+
+admin.initializeApp();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.json({message: "Hello from Firebase Functions!"});
+});
+
+// Firebase Functions を us-central1 に固定（無料プランでもデプロイ可能）
+exports.api = functions.https.onRequest(app);
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
